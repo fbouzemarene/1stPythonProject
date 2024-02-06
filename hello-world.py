22c66217615ec4f8d3fe6cf0c7246ece5289716f
Farid Bouzemarene <fbouzemarene@gmail.com>
2024-02-06 18:58:19 +0100

    Modify hello python file format

--- hello-world.py
+++ hello-world.py
@@ -2,13 +2,17 @@ import requests
 import json
 import pprint
 
-url = "https://api.mist.com/api/v1/sites/addd1bff-4b9c-44ee-a6c0-87920b7a9aa0/wlans"
-url2 = "https://api.mist.com/api/v1/sites/addd1bff-4b9c-44ee-a6c0-87920b7a9aa0/wlans"
-url3 = "https://api.mist.com/api/v1/sites/addd1bff-4b9c-44ee-a6c0-87920b7a9aa0/wlans"
+url = "https://api.mist.com/api/v1/sites/addd1bff-4b9c\
+-44ee-a6c0-87920b7a9aa0/wlans"
+url2 = "https://api.mist.com/api/v1/sites/addd1bff-4b9c\
+-44ee-a6c0-87920b7a9aa0/wlans"
+url3 = "https://api.mist.com/api/v1/sites/addd1bff-4b9c\
+-44ee-a6c0-87920b7a9aa0/wlans"
 
 headers = {
     "Content-Type": "application/json",
-    "Authorization": "ej09KqSG35Q2TzbodP76AHWRV4FcJNHx4kpSjFnR0GmJ4T3y3cjoqq070q9Si1ecGDAQYMwDC1o0n4aYCOcrXM8QbJR5KgtA",
+    "Authorization": "ej09KqSG35Q2TzbodP76AHWRV4FcJNHx4kpSj\
+    FnR0GmJ4T3y3cjoqq070q9Si1ecGDAQYMwDC1o0n4aYCOcrXM8QbJR5KgtA",
 }
 
 results = requests.get(url, headers=headers)
@@ -16,5 +20,5 @@ wlans = json.loads(results.text)
 
 pprint.pprint(wlans)
 
-for wlan in wlans:
-    print(wlan["ssid"], ">", wlan["id"])
+# for wlan in wlans:
+#    print(wlan["ssid"], ">", wlan["id"])
